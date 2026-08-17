f30552de65fee4390cfd008682fe809f9777c814
arpanosso <arpanosso@yahoo.com.br>
2026-08-17 11:34:36 -0300

    aula 1

---
+++ scripts/aula-01.R
@@ -0,0 +1,26 @@
+# Aula 01 - Experimentação Agronômica
+# Data - 17/08/2026
+# Programado por - Alan Rodrigo Panosso
+2+2 # soma
+25/5 # divisão
+exp(2) # exponencial natural
+
+# Criação de um Gráfico
+# 1 - Criar x
+x = 1:100
+
+# 2 - Criar y
+y = 2 + 1/2*x
+
+# 3 - Criar gráfico
+plot(x,y)
+
+# Soma de números reais
+2.5 + 5.5
+
+1223.42 # notação real
+1.22342e3 # notação científica e3 = 1000
+0.0000000000566
+
+# Dica - Tools/Global Options/CODE/Display/
+# selecionar Soft-wrape Source Files

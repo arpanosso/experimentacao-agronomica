1dbfd5a96b49615052b06a7f463a734ef3e0e825
Alan Rodrigo Panosso <arpanosso@yahoo.com.br>
2026-08-23 12:59:36 -0300

    listas 1 e 2

---
+++ scripts/aula-02.R
@@ -0,0 +1,12 @@
+# Aula 02 - Experimentação Agronômica
+# Data - 24/08/2026
+# Programado por - Alan Rodrigo Panosso
+
+# Resolver a Lista de Exercícios - 1
+
+# Lista 01 ----------------------------------------------------------------
+
+
+
+# Lista 02 ----------------------------------------------------------------
+
